1991379a0cf7d2b9a5c6d6ec5605b1c45f6630ab
VuXuanAn <anvu1308@gmail.com>
2025-07-29 21:45:20 +0700

    feat: init project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="h-[100vh] w-full grid place-items-center">
+    <span class="text-5xl font-bold"> vite work</span>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
